02a725c29fa6fd2a4df560fdbef421a6be3a2644
bonce <chris@headstrong.solutions>
2019-12-26 08:25:32 +0000

    Corrected framework version and fixed endpoint routing in startup

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace Headstrong
                 };
             });
 
-            services.AddMvc();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
